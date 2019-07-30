1718116415674bcc1f3f06dc6d839eb11258e6b1
Russ White <russ.white@lifway.com>
2019-07-30 12:42:26 -0500

    cleanup instance impl

--- core/src/main/scala/earlyeffect/DiodeComponent.scala
+++ core/src/main/scala/earlyeffect/DiodeComponent.scala
@@ -1,10 +1,11 @@
 package earlyeffect
 
 import diode.{Circuit, FastEq, ModelR}
-import earlyeffect.impl.{DiodeComponentInstance, EarlyEffect}
+import earlyeffect.impl.{ComponentJS, EarlyEffect, VNodeJS}
 
 import scala.language.implicitConversions
 import scala.scalajs.js
+import scala.scalajs.js.annotation.JSName
 
 trait DiodeComponent[Props, M <: AnyRef, State] { self =>
 
@@ -13,7 +14,7 @@ trait DiodeComponent[Props, M <: AnyRef, State] { self =>
 
   def zoom(get: M => State)(implicit f: FastEq[_ >: State]): ModelR[M, State] = circuit.zoom(get)
 
-  type I = DiodeComponentInstance[Props, M, State]
+  type I = DiodeComponent.Instance[Props, M, State]
   def render(props: Props, state: State): VNode
 
   def didMount(instance: I): Unit                                    = ()
@@ -35,4 +36,53 @@ trait DiodeComponent[Props, M <: AnyRef, State] { self =>
         ("cc", self.asInstanceOf[js.Any])
       )
     )
+}
+
+object DiodeComponent {
+
+  class Instance[Props, M <: AnyRef, State] extends ComponentJS {
+
+    type CM     = Circuit[M]
+    type Reader = Props => ModelR[M, State]
+
+    private var unsubscribe: () => Unit = () => ()
+
+    def circuit: CM = component().circuit
+
+    def reader: Reader = component().reader
+
+    def component(props: js.Dynamic = rawProps): earlyeffect.DiodeComponent[Props, M, State] =
+      props.cc.asInstanceOf[earlyeffect.DiodeComponent[Props, M, State]]
+
+    def lookup(props: js.Dynamic = rawProps): Props = props.p1.asInstanceOf[Props]
+
+    @JSName("richProps")
+    def props = lookup()
+
+    def putState(state: State): Unit = rawSetState(js.Dynamic.literal(s = state.asInstanceOf[js.Any]))
+
+    def fetchState(raw: js.Dynamic = this.rawState): State = raw.s.asInstanceOf[State]
+
+    override def render(props: js.Dynamic, state: js.Dynamic): VNodeJS =
+      component(props).render(lookup(props), fetchState(state)).vn
+
+    def componentDidMount(): Unit =
+      component().didMount(this)
+
+    def componentWillMount(): Unit = {
+      component().willMount(this)
+      this.putState(reader(props).value)
+      unsubscribe = circuit.subscribe(reader(props))(r => { this.putState(r.value) })
+    }
+
+    def componentWillUnmount() = unsubscribe()
+
+    def componentDidUpdate(oldProps: js.Dynamic, oldState: js.Dynamic, snapshot: js.Dynamic): Unit =
+      component().didUpdate(lookup(oldProps), fetchState(oldState), this)
+
+    def shouldComponentUpdate(nextProps: js.Dynamic, nextState: js.Dynamic, context: js.Dynamic): Boolean =
+      component().shouldUpdate(lookup(nextProps), fetchState(nextState), this)
+
+  }
+
 }
