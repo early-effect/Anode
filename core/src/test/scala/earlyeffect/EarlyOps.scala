f94cc30c15bb690cd8f2701913161fc6351c0f79
Russ White <russ.white@lifway.com>
2019-08-27 18:36:43 -0500

    cleanup

--- core/src/test/scala/earlyeffect/EarlyOps.scala
+++ core/src/test/scala/earlyeffect/EarlyOps.scala
@@ -6,10 +6,9 @@ import org.scalajs.dom.Element
 trait EarlyOps {
   def render(vn: VNode): Unit = preact.render(vn, div)
 
-  lazy val div: Element = {
+  val div: Element = {
     val res = dom.document.createElement("div")
     dom.document.documentElement.appendChild(res)
     res
   }
-  def replacedNode: Element = div.querySelector("span")
 }
