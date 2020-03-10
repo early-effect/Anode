12134ba4cbd1d4acfe2a8f9928ea16dfa4f0e9c7
Russ White <russwyte@users.noreply.github.com>
2020-03-10 14:49:45 -0500

    When should be lazy...

--- core/src/main/scala/earlyeffect/When.scala
+++ core/src/main/scala/earlyeffect/When.scala
@@ -1,10 +1,9 @@
 package earlyeffect
 
-class When(p: Boolean) {
-  def apply[A, B <: A](a: A, b: B): A = if (p) a else b
-  def apply(c: => Child): Child       = apply(c, EmptyChild)
-  def apply(a: => Arg): Arg           = apply(a, Empty)
-
+class When(p: => Boolean) {
+  def apply[A, B <: A](a: => A, b: B): A = if (p) a else b
+  def apply(c: => Child): Child          = apply(c, EmptyChild)
+  def apply(a: => Arg): Arg              = apply(a, Empty)
 }
 
 object When {
