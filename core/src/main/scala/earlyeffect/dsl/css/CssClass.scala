b3108ed8536b5fc2f9170a593a60242a3a7ff9bb
Russ White <russwyte@users.noreply.github.com>
2020-09-15 08:51:34 -0500

    work around for testing components where there is no DOM

--- core/src/main/scala/earlyeffect/dsl/css/CssClass.scala
+++ core/src/main/scala/earlyeffect/dsl/css/CssClass.scala
@@ -2,10 +2,10 @@ package earlyeffect.dsl.css
 
 import earlyeffect.dsl.css.Styles.{DeclarationOrSelector, KeyFrames, MediaQuery, Selector}
 import earlyeffect.{Attribute, ClassSelector}
-import org.scalajs.dom.html.Document
 import org.scalajs.dom.raw.Element
 
 import scala.scalajs.js
+import scala.scalajs.js.JavaScriptException
 
 abstract class CssClass(ds: DeclarationOrSelector*) extends Attribute { self =>
   override val name                       = "class"
@@ -14,7 +14,6 @@ abstract class CssClass(ds: DeclarationOrSelector*) extends Attribute { self =>
   lazy val selector                       = s".$className"
   override val value                      = className.asInstanceOf[js.Any]
   lazy val sel: Selector                  = Selector(selector, members: _*)
-  private def doc: Option[Document]       = Option(org.scalajs.dom.document)
 
   def mkString: String = {
     val keyframes    = js.Array[KeyFrames]()
@@ -30,13 +29,16 @@ abstract class CssClass(ds: DeclarationOrSelector*) extends Attribute { self =>
   }
 
   private def appendStyle(): Unit =
-    for {
-      d <- doc
-    } {
+    try {
+      val d              = org.scalajs.dom.document
       val style: Element = d.createElement("style")
       style.setAttribute("data-style-for", className)
       style.appendChild(d.createTextNode(AutoPrefixed(mkString)))
       d.head.appendChild(style)
+    } catch {
+      // this should only happen when there is no DOM - like in certain test runners.
+      case e: Throwable => ()
     }
+
   self.appendStyle()
 }
