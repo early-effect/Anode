89783a11db7714ec6ade58c6c5e939ef5fdd520f
Russ White <russwyte@users.noreply.github.com>
2021-01-30 10:36:38 -0600

    use readonly test

--- demo-app/src/main/scala/todo/Creator.scala
+++ demo-app/src/main/scala/todo/Creator.scala
@@ -6,7 +6,7 @@ import org.scalajs.dom.raw.HTMLInputElement
 import todo.model.TodoList.actions.Add
 import todo.model.{Todo, TodosCircuit}
 
-object Creator extends Component[Unit] {
+object Creator extends StatefulComponent[Unit, Boolean] {
 
   object Input
       extends StyledElement(E.input)(
@@ -26,9 +26,13 @@ object Creator extends Component[Unit] {
         Selector("::placeholder", fontStyle.italic, fontWeight(300), color.rgba(75, 25, 25, .5)),
       )
 
-  override def render(props: Unit): VNode =
+  override def initialState(props: Unit): Boolean = true
+
+  override def render(props: Unit, state: Boolean, instance: Creator.Instance): VNode =
     Input(
+      A("readonly", state),
       A.placeholder("What needs to be done?"),
+      A.onClick(_ => instance.setState(false)),
       A.onKeyDown { x =>
         val e = x.target.asInstanceOf[HTMLInputElement]
         if (x.keyCode == ENTER && e.value.nonEmpty) {
@@ -37,4 +41,5 @@ object Creator extends Component[Unit] {
         }
       },
     )
+
 }
